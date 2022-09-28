063c7a99e38c7d5c17c9fb5e71507e3ffb464abc
Andrei Bogdanov <xbas78@gmail.com>
2022-09-28 19:01:18 +0300

    initial

---
+++ imagebutton.cpp
@@ -0,0 +1,41 @@
+#include "imagebutton.h"
+
+  ImageButton::ImageButton(QWidget *parent) {
+    setParent(parent);
+    setToolTip("Stop");
+    setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    mButtonUpPixmap = QPixmap("../red_button_up.png");
+    mButtonDownPixmap = QPixmap("../red_button_down.png");
+    mCurrentButtonPixmap = mButtonUpPixmap;
+    setGeometry(mCurrentButtonPixmap.rect());
+    connect(this, &QPushButton::clicked,
+            this, &ImageButton::setDown);
+  }
+
+  void ImageButton::paintEvent(QPaintEvent *e) {
+    QPainter p(this);
+    p.drawPixmap(e->rect(), mCurrentButtonPixmap);
+  }
+
+  [[nodiscard]] QSize ImageButton::sizeHint() const {
+    return {100, 100};
+  }
+
+  [[nodiscard]] QSize ImageButton::minimumSizeHint() const {
+    return sizeHint();
+  }
+
+  void ImageButton::keyPressEvent(QKeyEvent *e) {
+    setDown();
+  }
+
+  void ImageButton::setDown() {
+    mCurrentButtonPixmap = mButtonDownPixmap;
+    update();
+    QTimer::singleShot(100, this, &ImageButton::setUp);
+  }
+
+  void ImageButton::setUp() {
+    mCurrentButtonPixmap = mButtonUpPixmap;
+    update();
+  }
---
+++ imagebutton.h
@@ -0,0 +1,29 @@
+#include <QPixmap>
+#include <QPainter>
+#include <QPaintEvent>
+#include <QTimer>
+#include <QMediaPlayer>
+#include <QPushButton>
+
+class ImageButton : public QPushButton {
+  QPixmap mCurrentButtonPixmap;
+  QPixmap mButtonDownPixmap;
+  QPixmap mButtonUpPixmap;
+
+public:
+  ImageButton() = default;
+
+  explicit ImageButton(QWidget *parent);
+
+  void paintEvent(QPaintEvent *e) override;
+
+  [[nodiscard]] QSize sizeHint() const override;
+
+  [[nodiscard]] QSize minimumSizeHint() const override;
+
+  void keyPressEvent(QKeyEvent *e) override;
+
+  void setDown();
+
+  void setUp();
+};
